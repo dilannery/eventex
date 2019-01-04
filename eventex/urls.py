80f89fb5894a8eac1aa696d6782e1c3d232c95b5
Dilan Nery <dnerylopes@gmail.com>
2019-01-04 15:34:05 -0300

    :sparkles: Create 'home' view

--- eventex/urls.py
+++ eventex/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+import eventex.core.views
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url('', eventex.core.views.home),
+    url('admin/', admin.site.urls),
 ]

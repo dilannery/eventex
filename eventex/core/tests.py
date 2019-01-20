337823170f04fec76a9dde10f146a32ddee5d5ff
Dilan Nery <dnerylopes@gmail.com>
2019-01-20 01:04:20 -0300

    :white_check_mark: Add home tests

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class HomeTest(TestCase):
+    def setUp(self):
+        self.request = self.client.get('/')
+
+    def test_get(self):
+        """GET / must return status code 200"""
+        self.assertEqual(200, self.request.status_code)
+
+    def test_template(self):
+        """GET / must use index.html"""
+        self.assertTemplateUsed(self.request, 'index.html')
